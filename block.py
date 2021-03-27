819b58aac50df9887fd1d4c4787e378e9399cf17
github username <dirtyassweed@icloud.com>
2021-03-27 13:19:42 -0400

    refactor

--- block.py
+++ block.py
@@ -1,18 +1,6 @@
 import time
 
 
-def mine_block(last_block, data):
-    timestamp = time.time_ns()
-    last_hash = last_block.hash
-    hash = f'{timestamp}-{last_hash}'
-
-    return Block(timestamp, last_hash, hash, data)
-
-
-def genesis():
-    return Block(1, 'genesis_last_hash', 'genesis_hash', [])
-
-
 class Block:
 
     def __init__(self, timestamp, last_hash, hash, data):
@@ -30,10 +18,22 @@ class Block:
             f'Block data: {self.data})'
         )
 
+    @staticmethod
+    def mine_block(last_block, data):
+        timestamp = time.time_ns()
+        last_hash = last_block.hash
+        hash = f'{timestamp}-{last_hash}'
+
+        return Block(timestamp, last_hash, hash, data)
+
+    @staticmethod
+    def genesis():
+        return Block(1, 'genesis_last_hash', 'genesis_hash', [])
+
 
 def main():
-    genesis_block = genesis()
-    block = mine_block(genesis_block, 'foo')
+    genesis_block = Block.genesis()
+    block = Block.mine_block(genesis_block, 'foo')
     print(block)
 
 
